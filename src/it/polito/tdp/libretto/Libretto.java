691a0d13e60618aa230f3285d266c0c4df358544
Flavio <Flavio@Lillo>
2019-03-14 17:10:04 +0100

    lib

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,21 @@
  package it.polito.tdp.libretto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
+	    // uso un'interfaccia
+	private List <Voto> voti;
+	
+	public Libretto() {
+		           // devo necessariamente usare una classe
+		voti = new ArrayList <Voto>();
+		
+		
+	}
+	
+	public void add(Voto v) {
+		 
+	}
 
 }
